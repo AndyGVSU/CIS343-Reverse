a63ff0287c0eab3919d68edc321abf60c58d65cc
Andy <hudsoand@mail.gvsu.edu>
2018-09-11 21:04:48 -0400

    Project functions properly, no code cleanup

--- reverse.c
+++ reverse.c
@@ -22,17 +22,52 @@ int main(int argc, char** argv)
 	//printf("%c",bufferPointer[1]);
 	//reverse string in buffer
 
-	printf("%c",*fileBuffer[0]);
+	//printf("%c",*fileBuffer[0]);
 
-	//int reverse = bytesRead / 2;
+	int first = 0;
+	int last = bytesRead - 2; //for array bound / null character
+	char swapCharFirst;
+	char swapCharLast;
+
 	//char* reverse = (char*) malloc(bytesRead * sizeof(char));
-	/*
-	for (int i = 0; i < (bytesRead / 2) - 1; i++)
+	
+	//*fileBuffer += sizeof(char) * middle;
+
+	while (first < last)
 		{
+		//store first char
+		swapCharFirst = **fileBuffer;
+		//goto last char
+		*fileBuffer += sizeof(char) * (last - first);
+		//store last char
+		swapCharLast = **fileBuffer;
+		//set last char to stored first char
+		**fileBuffer = swapCharFirst;
+		//goto first char
+		*fileBuffer -= sizeof(char) * (last - first);
+		//set first char to stored last char
+		**fileBuffer = swapCharLast;
 		
+		//move buffer forward
+		*fileBuffer += sizeof(char);
+		//close bounds
+		first++;
+		last--;
+		}
+	//move buffer back to original position
+	*fileBuffer -= first * sizeof(char);
+	
+	//printf("%s",*fileBuffer);
+	/*
+	for (int i = 0; i < (bytesRead); i++)
+		{
+		printf("%c",**fileBuffer);
+		printf("%d",i);
+		*fileBuffer += sizeof(char);
 		//reverse[i] = *fileBuffer[bytesRead - i - 1];
 		}
 	*/
+	//*fileBuffer -= (sizeof(char) * bytesRead);
 
 	int bytesWritten = write_file(outputFilename, *fileBuffer, bytesRead);
 	//printf("%d",bytesWritten);
